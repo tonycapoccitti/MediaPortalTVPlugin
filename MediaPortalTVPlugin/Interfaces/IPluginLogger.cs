15c6a7dc66c12eebc9c8c9e61f091d88f4c8871e
ChubbyArse <ads@chubbymusic.co.uk>
2014-11-04 20:27:02 +0000

    Ads

---
+++ MediaPortalTVPlugin/Interfaces/IPluginLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPortalTVPlugin.Interfaces
+{
+    /// <summary>
+    /// Provides plugin specific implementations for logging
+    /// </summary>
+    public interface IPluginLogger
+    {
+        /// <summary>
+        /// Logs an exception, with a message and detail
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <param name="message"></param>
+        /// <param name="args"></param>
+        void LogError(Exception ex, String message, params Object[] args);
+    }
+}
