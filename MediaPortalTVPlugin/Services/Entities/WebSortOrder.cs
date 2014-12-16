d9a328054b0f45f41b4ee12761d366fd68241531
ChubbyArse <ads@chubbymusic.co.uk>
2014-12-16 19:40:53 +0000

    Added missing file

---
+++ MediaPortalTVPlugin/Services/Entities/WebSortOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBrowser.Plugins.MediaPortal.Services.Entities
+{
+    public enum WebSortOrder
+    {
+        Asc = 0,
+        Desc = 1,
+        Custom = 2
+    }
+}
